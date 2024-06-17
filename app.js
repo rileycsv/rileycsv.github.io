9391978bbf10c4a18cc72df1971c861c8c50c310
Riley <160427476+rileycsv@users.noreply.github.com>
2024-06-17 11:43:30 -0400

    Delete app.js

--- app.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener('DOMContentLoaded', function() {
-    const introText = document.querySelector('.special');
-    
-    // Delay the animation start
-    setTimeout(function() {
-        introText.style.opacity = '1'; // Fade in by setting opacity to 1
-    }, 500); // Adjust the delay as needed
-});
